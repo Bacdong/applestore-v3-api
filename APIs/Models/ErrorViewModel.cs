aaea29736d02d2255361d5190b08994118700e12
Bacdong <duongdong2203@gmail.com>
2020-10-22 15:42:59 +0700

    Create APIs project

---
+++ APIs/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace APIs.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
